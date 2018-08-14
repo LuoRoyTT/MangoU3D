979a4e8b0a3d027a6849e007eab445d5e232ef90
LuoRoyTT <32155590+LuoRoyTT@users.noreply.github.com>
2018-08-15 01:00:37 +0800

    8.14

---
+++ Assets/Scripts/Data/RecyclableData.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Client.Data
+{
+	public abstract class RecyclableData 
+	{ 
+		public abstract string ClassKey{get;}
+		public virtual void OnUse()
+		{
+		}
+
+		public virtual void OnRelease()
+		{
+		}
+
+	}
+}
---
+++ Assets/Scripts/Data/RecyclableDataPool.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+using UnityEngine;
+
+namespace Client.Data
+{
+
+    public class RecyclableDataPool
+    {
+		public Type type;
+		public string classKey;
+		public Stack<RecyclableData> unusedobjs = new Stack<RecyclableData>();
+		private static Dictionary<string,RecyclableDataPool> poolMaps = new Dictionary<string, RecyclableDataPool>();
+		public RecyclableDataPool()
+		{
+			Init();
+		}
+		public RecyclableDataPool(Type type,string classKey)
+		{
+			this.type=type;
+			this.classKey=classKey;
+		}
+		public static void Init()
+		{
+			if (RecyclableDataPool.poolMaps.Count > 0)
+			{
+				return;
+			}
+			Type typeFromHandle = typeof(RecyclableDataPool);
+			Type[] types = typeFromHandle.Assembly.GetTypes();
+			for (int i = 0; i < types.Length; i++)
+			{
+				Type type = types[i];
+				if (!type.IsAbstract && type.IsSubclassOf(typeFromHandle))
+				{
+					FieldInfo field = type.GetField("CLASS_KEY", BindingFlags.Static|BindingFlags.Public);
+					string key = (string)field.GetValue(null);
+					RecyclableDataPool pool = new RecyclableDataPool(type,key);
+					if(!RecyclableDataPool.poolMaps.ContainsKey(pool.classKey))
+					{
+						RecyclableDataPool.poolMaps.Add(pool.classKey,pool);
+					}
+				}
+			}
+		}
+
+		public static RecyclableData Get(string classKey)
+		{
+			RecyclableDataPool pool;
+			if(poolMaps.TryGetValue(classKey,out pool))
+			{
+				if(pool.unusedobjs.Count>0)
+				{
+					return pool.unusedobjs.Pop();
+				}
+				else
+				{
+					return (RecyclableData)Activator.CreateInstance(pool.type);
+				}
+			}
+			else
+			{
+				Debug.LogError("不存在CLASS_KEY:"+classKey);
+				return null;
+			}
+
+		}
+		public static RecyclableDataPool GetPool(string classKey)
+		{
+			return poolMaps[classKey];
+		}
+	    public static void Release(RecyclableData recyclableData)
+        {
+            
+        }
+		public static void Clear(RecyclableDataPool pool)
+		{
+			
+		}
+    }
+}
