d081c831af9438077bbcc1e0298bfe2d9102ed24
LuoRoyTT <32155590+LuoRoyTT@users.noreply.github.com>
2018-08-16 22:41:39 +0800

    8.16

--- Assets/Scripts/ResourceModule/ResourceModule.cs
+++ Assets/Scripts/ResourceModule/ResourceModule.cs
@@ -1,13 +1,25 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using Client.Core;
 using UnityEngine;
 
 namespace Client.ResourceModule
-{
+{ 
 	public class ResourceModule : MonoSingleton<ResourceModule> 
 	{
-
+		private Dictionary<string,IAssetLoader> loadersMap = new Dictionary<string,IAssetLoader>();
+		public T Create<T>(string assetname) where T:IAssetLoader
+        {
+			if(loadersMap.ContainsKey(assetname))
+			{
+				return (T)loadersMap[assetname];
+			}
+			else
+			{
+				return (T)Load(assetname) 
+			}
+		}
 
 	}
 }
