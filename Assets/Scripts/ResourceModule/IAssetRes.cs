543e133e004121cbe38622ac46d579ba530aff27
LuoRoyTT <32155590+LuoRoyTT@users.noreply.github.com>
2018-09-04 21:47:03 +0800

    9.4

---
+++ Assets/Scripts/ResourceModule/IAssetRes.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Client.ResourceModule
+{
+	public interface IAssetAsynLoader
+	{
+		AssetBundleRequest request{get;}
+	}
+	public interface IAssetRes
+	{
+		string AssetName{get;}
+		void Init(string assetName);
+		T Load<T>() where T:UnityEngine.Object; 
+		void LoadAsyn<T>(Action<T> onCacheFinished) where T:UnityEngine.Object;
+		IAssetAsynLoader LoadAsyn();
+		void Recycle();
+	} 
+}
---
+++ Assets/Scripts/ResourceModule/ResourceManager.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Client.Core;
+using Client.Data;
+using Client.FuncAllocator;
+using UnityEngine;
+
+namespace Client.ResourceModule
+{ 
+	public class ResourceManager : MonoSingleton<ResourceManager> 
+	{
+
+		private Dictionary<string,IAssetRes> loadersMap;
+		private List<IAssetRes> waitForReleaseLoaders;
+		private float releaseInterval = 10f;
+		private FuncRec releaseLoaderFuncRec;
+		public AssetBundleManifest Manifest{get;private set;}
+		private static string LOAD_TYPE_CLASS_KEY;
+		protected override void Init()
+		{
+			LOAD_TYPE_CLASS_KEY = "AssetBundleRes";
+		}
+
+		public IAssetRes Get(string assetName)
+		{
+			if(loadersMap.ContainsKey(assetName))
+			{
+				return loadersMap[assetName];
+			}
+			else
+			{
+				int index = waitForReleaseLoaders.FindIndex((a)=>{return a.AssetName.Equals(assetName);});
+				if(index!=-1)
+				{
+					IAssetRes loader = waitForReleaseLoaders[index];
+					waitForReleaseLoaders.Remove(loader);
+					return loader;
+				}
+				else
+				{
+					return CreateLoader(assetName);
+				}
+			}
+		}
+
+		private IAssetRes CreateLoader(string assetName) 
+		{
+			IAssetRes loader = RecyclableObjectPool.Get(LOAD_TYPE_CLASS_KEY) as IAssetRes;
+			loader.Init(assetName);
+			return loader;
+		}
+		public bool Exist(string assetName)
+		{
+			return loadersMap.ContainsKey(assetName);
+		}
+	}
+}
+
