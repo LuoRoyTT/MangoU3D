0264930442f2b3e20769367d798da7baaacc2182
LuoRoyTT <32155590+LuoRoyTT@users.noreply.github.com>
2018-09-30 21:11:49 +0800

    9.30

---
+++ Assets/Mango/Framework.Core/MCoroutine.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Mango.Framework.Core
+{
+	public class MCoroutine  
+	{
+		public IEnumerator iter;
+
+		public MCoroutine(IEnumerator it)
+		{
+			this.iter = it;
+		}
+
+	}
+}
+
